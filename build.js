7a700c50b23200e6b24811bb43b5d09d5d113fb8
Dryad Oak <dryad-oak@hotmail.co.uk>
2023-12-10 13:00:45 +0000

    refactored build script and fixed formatting issue in html render

--- build.js
+++ build.js
@@ -1,84 +1,82 @@
-const fs = require("fs");
+const fs = require("fs").promises;
 const path = require("path");
 const ejs = require("ejs");
 const headPageData = require("./headPageData.js");
 
-function ejs2html({ filePath, outPath, data, options }) {
-  ejs.renderFile(
-    filePath,
-    { ...data, ...options }, // Pass both data and options to the template
-    {
-      filename: filePath,
-      views: [path.join(__dirname, 'views')],
-      layout: 'layouts/layout',
-      partials: { partialsDir: path.join(__dirname, 'views/partials') },
-    },
-    (err, html) => {
-      if (err) {
-        console.log("Error during rendering:", err);
-        return false;
-      }
-    //   layouts config!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-      const fileNameNoExt = path.parse(filePath).name
-      const layoutContent = { body: html, headPageData: headPageData[fileNameNoExt]};
-
-      ejs.renderFile(
-        path.join(__dirname, 'views/layouts/layout.ejs'),
-        layoutContent,
-        { filename: path.join(__dirname, 'views/layouts/layout.ejs') },
-        (err, layoutHtml) => {
-          if (err) {
-            console.log("Error during rendering layout:", err);
-            return false;
-          }
-
-          fs.writeFile(outPath, layoutHtml, function (err) {
-            if (err) {
-              console.log("Error during writing:", err);
-              return false;
-            }
-            console.log(`File ${outPath} has been saved.`);
-          });
-        }
-      );
+class PageBuilder {
+    constructor() {
+        this.baseDir = path.join(__dirname, "views");
+        this.outputDir = path.join(__dirname, "public");
+        this.layoutFilePath = path.join(__dirname, "views/layouts/layout.ejs");
+        this.imagesSrcsetDataPath = "responsiveImageGenerator/images.json";
     }
-  );
+     // Initialize the PageBuilder
+    async init() {
+        try {
+            const files = await fs.readdir(this.baseDir);
+            const ejsFiles = files.filter(
+                (file) => path.extname(file) === ".ejs",
+            );
+            this.imagesSrcsetData = JSON.parse(
+                await fs.readFile(this.imagesSrcsetDataPath, "utf8"),
+            );
+
+            for (const fileName of ejsFiles) {
+                await this.buildPage(fileName);
+            }
+        } catch (err) {
+            console.error("Error during initialization:", err);
+        }
+    }
+    // Build a page using EJS template
+    async buildPage(fileName) {
+        try {
+            const ejsFilePath = path.join(this.baseDir, fileName);
+            const htmlOutputPath = path.join(
+                this.outputDir,
+                path.parse(fileName).name + ".html",
+            );
+
+            const html = await this.renderFile(ejsFilePath, {
+                images: this.imagesSrcsetData,
+                views: [path.join(__dirname, "views")],
+                layout: "layouts/layout",
+                partials: {
+                    partialsDir: path.join(__dirname, "views/partials"),
+                },
+            });
+
+            const fileNameNoExt = path.parse(fileName).name;
+            const layoutHtml = await this.renderLayout(html, fileNameNoExt);
+            await this.writeFile(htmlOutputPath, layoutHtml);
+        } catch (err) {
+            console.error("Error building page:", err);
+        }
+    }
+    // Render an EJS file
+    async renderFile(filePath, data) {
+        try {
+            return await ejs.renderFile(filePath, data);
+        } catch (err) {
+            throw err;
+        }
+    }
+     // Render the layout with the provided bodyHtml
+    async renderLayout(bodyHtml, fileNameNoExt) {
+        const layoutOptions = {
+            body: bodyHtml,
+            headPageData: headPageData[fileNameNoExt],
+        };
+
+        return this.renderFile(this.layoutFilePath, layoutOptions);
+    }
+    // Write content to a file
+    async writeFile(outPath, content) {
+        await fs.writeFile(outPath, content);
+        console.log(`File ${outPath} has been saved.`);
+    }
 }
 
-
-
-const baseDir = __dirname + '/views';
-const outputDir = __dirname + '/public';
-
-// Read all files in the views directory
-fs.readdir(baseDir, (err, files) => {
-  if (err) {
-    console.error('Error reading directory:', err);
-    return;
-  }
-
-  // Filter out only the EJS files
-  const ejsFiles = files.filter(file => path.extname(file) === '.ejs');
-
-    // Read the images.json file
-  const imagesSrcsetDataPath = "responsiveImageGenerator/images.json";
-  const imagesSrcsetData = JSON.parse(
-    fs.readFileSync(imagesSrcsetDataPath, "utf8"),
-  );
-
-
-
-  ejsFiles.forEach((fileName) => {
-    const ejsFilePath = path.join(baseDir, fileName);
-    const htmlOutputPath = path.join(outputDir, path.parse(fileName).name + '.html');
-
-    ejs2html({
-      filePath: ejsFilePath,
-      outPath: htmlOutputPath,
-      options: {
-        images: imagesSrcsetData,
-        // Add any other options you need
-    },
-    });
-  });
-});
+// Usage
+const pageBuilder = new PageBuilder();
+pageBuilder.init();
