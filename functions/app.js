b3b0438a0f2dca01b6810cae0f24c0aaa301355d
Dryad Oak <dryad-oak@hotmail.co.uk>
2024-01-06 15:22:29 +0000

    changed order of recap validation

--- functions/app.js
+++ functions/app.js
@@ -22,12 +22,12 @@ app.use(express.urlencoded({ extended: true }));
 
 
 
-router.post("/contact", contactFormValidationRules, validationMiddleware, reCaptchaValidation, (req, res) => {
+router.post("/contact", reCaptchaValidation, contactFormValidationRules, validationMiddleware, (req, res) => {
     sanitizeFormData(req);
     sendFormToEmail(req, res, "contact");
 });
 
-router.post("/", reviewFormValidationRules, validationMiddleware, reCaptchaValidation, (req, res) => {
+router.post("/", reCaptchaValidation, reviewFormValidationRules, validationMiddleware, (req, res) => {
     sanitizeFormData(req);
     sendFormToEmail(req, res, "review");
 });
